bdbdedb57975a8a2fe6f5b3f11c26519e406e04a
Salman Karim <salmankarim.khan42@gmail.com>
2022-12-06 19:12:51 +0500

    RN-Assignment done

--- src/Config/Screens/itemDetails.js
+++ src/Config/Screens/itemDetails.js
@@ -1,171 +1,164 @@
 import axios from 'axios';
-import React, { useEffect, useState } from 'react';
+import React, {useEffect, useState} from 'react';
 import {
-    Button,
-    SafeAreaView,
-    StyleSheet,
-    Text,
-    TextInput,
-    View,
-    ImageBackground,
-    TouchableOpacity,
-    ScrollView,
-    Image,
+  Button,
+  SafeAreaView,
+  StyleSheet,
+  Text,
+  TextInput,
+  View,
+  ImageBackground,
+  TouchableOpacity,
+  ScrollView,
+  Image,
 } from 'react-native';
 import globalStyles from '../../Utils/globalStyles';
 
-export default function ItemDetails({ Navigation, route }) {
+export default function ItemDetails({Navigation, route}) {
+  const selectedItemId = route.params[0];
+  const selectedItem = route.params[1].filter(e => e.id === selectedItemId);
+  const sameCatList = route.params[1].filter(e => e.id !== selectedItemId);
 
-    const selectedItemId = route.params[0]
-    const selectedItem = route.params[1].filter(e => e.id === selectedItemId)
-    const sameCatList = route.params[1].filter(e => e.id !== selectedItemId)
+  console.log(selectedItem);
 
-    console.log(selectedItem);
+  const [loader, setloader] = useState(false);
 
-    const [loader, setloader] = useState(false);
-
-    const cusChip = val => {
-        return (
-            <View style={globalStyles.chipView}>
-                <Text style={globalStyles.chipText}>more</Text>
-            </View>
-        );
-    };
-    const titleHandler = val => {
-        const title = val.length > 12 ? val.slice(0, 12) + '...' : val;
-        return (
-            <View
-                style={{
-                    justifyContent: 'space-between',
-                    backgroundColor: 'yellow',
-                    width: '100%',
-                    flex: 3,
-                    paddingHorizontal: 6,
-                    paddingBottom: 6,
-                }}>
-                <Text style={globalStyles.text}>{title}</Text>
-                {cusChip(val)}
-            </View>
-        );
-    };
-
-
+  const cusChip = val => {
     return (
-        <SafeAreaView style={globalStyles.mainView}>
-            <View style={globalStyles.headerView}>
-                <ImageBackground
-                    style={globalStyles.headerImage}
-                    source={require('../../Utils/Images/apiHeader.jpg')}></ImageBackground>
-            </View>
-            <View style={globalStyles.bodyView}>
-                {loader ? (
-                    <Image
-                        style={{
-                            width: '40%',
-                            aspectRatio: 1,
-                            marginHorizontal: '45%',
-                            marginTop: '35%',
-                        }}
-                        source={require('../../Utils/Images/loader04.gif')}
-                    />
-                ) : sameCatList.length > 0 ? (
-                    <ScrollView style={globalStyles.scrollView}>
-                        <Text style={globalStyles.categoryHeading}>
-                            {selectedItem[0].category}
-                        </Text>
-                        <View style={styles.selectedItemView}>
-                            <View style={styles.selectedItemUpperView}>
-                                <View style={styles.selectedItemLeftView}>
-                                    <Image
-                                        resizeMode="contain"
-                                        style={styles.selectedItemImage}
-                                        source={{ uri: `${selectedItem[0].image}` }}
-                                    />
-                                </View>
-                                <View style={styles.selectedItemRightView}>
-                                    <Text style={globalStyles.price}>
-                                        ${selectedItem[0].price}
-                                    </Text>
-                                    <Text style={globalStyles.text}>
-                                        {selectedItem[0].title}
-                                    </Text>
-                                </View>
-                            </View>
-                            <ScrollView style={styles.selectedItemLowerView}>
-                                <View>
-                                    <Text style={{ fontSize: 14, fontWeight: '600' }}>
-                                        Description:
-                                    </Text>
-                                    <Text style={globalStyles.text}>
-                                        {selectedItem[0].description}
-                                    </Text>
-                                </View>
-                            </ScrollView>
-                        </View>
-                        <View style={[globalStyles.cardView]}>
-                            {sameCatList.map((e, i) => (
-                                <TouchableOpacity
-                                    style={[globalStyles.cardUnit]}
-                                    key={i}
-                                    onpress={() => onSameCatPressHandler(e.id)}
-
-                                >
-                                    <View style={{ justifyContent: 'space-between', flex: 7 }}>
-                                        <Image
-                                            resizeMode="contain"
-                                            style={globalStyles.image}
-                                            source={{ uri: `${e.image}` }}
-                                        />
-                                        <Text style={globalStyles.price}>${e.price}</Text>
-                                    </View>
-                                    {titleHandler(e.title)}
-                                </TouchableOpacity>
-                            ))
-                            }
-                        </View>
-                    </ScrollView>
-                ) : (
-                    <Text style={{ alignSelf: 'center', alignContent: 'center' }}>
-                        no data found
-                    </Text>
-                )}
-            </View>
-        </SafeAreaView >
+      <View style={globalStyles.chipView}>
+        <Text style={globalStyles.chipText}>more</Text>
+      </View>
     );
+  };
+  const titleHandler = val => {
+    const title = val.length > 12 ? val.slice(0, 12) + '...' : val;
+    return (
+      <View
+        style={{
+          justifyContent: 'space-between',
+          backgroundColor: 'yellow',
+          width: '100%',
+          flex: 3,
+          paddingHorizontal: 6,
+          paddingBottom: 6,
+        }}>
+        <Text style={globalStyles.text}>{title}</Text>
+        {cusChip(val)}
+      </View>
+    );
+  };
+
+  return (
+    <SafeAreaView style={globalStyles.mainView}>
+      <View style={globalStyles.headerView}>
+        <ImageBackground
+          style={globalStyles.headerImage}
+          source={require('../../Utils/Images/apiHeader.jpg')}></ImageBackground>
+      </View>
+      <View style={globalStyles.bodyView}>
+        {loader ? (
+          <Image
+            style={{
+              width: '40%',
+              aspectRatio: 1,
+              marginHorizontal: '45%',
+              marginTop: '35%',
+            }}
+            source={require('../../Utils/Images/loader04.gif')}
+          />
+        ) : sameCatList.length > 0 ? (
+          <ScrollView style={globalStyles.scrollView}>
+            <Text style={globalStyles.categoryHeading}>
+              {selectedItem[0].category}
+            </Text>
+            <View style={styles.selectedItemView}>
+              <View style={styles.selectedItemUpperView}>
+                <View style={styles.selectedItemLeftView}>
+                  <Image
+                    resizeMode="contain"
+                    style={styles.selectedItemImage}
+                    source={{uri: `${selectedItem[0].image}`}}
+                  />
+                </View>
+                <View style={styles.selectedItemRightView}>
+                  <Text style={globalStyles.price}>
+                    ${selectedItem[0].price}
+                  </Text>
+                  <Text style={globalStyles.text}>{selectedItem[0].title}</Text>
+                </View>
+              </View>
+              <ScrollView style={styles.selectedItemLowerView}>
+                <View>
+                  <Text style={{fontSize: 14, fontWeight: '600'}}>
+                    Description:
+                  </Text>
+                  <Text style={globalStyles.text}>
+                    {selectedItem[0].description}
+                  </Text>
+                </View>
+              </ScrollView>
+            </View>
+            <View style={[globalStyles.cardView]}>
+              {sameCatList.map((e, i) => (
+                <TouchableOpacity
+                  style={[globalStyles.cardUnit]}
+                  key={i}
+                  onpress={() => onSameCatPressHandler(e.id)}>
+                  <View style={{justifyContent: 'space-between', flex: 7}}>
+                    <Image
+                      resizeMode="contain"
+                      style={globalStyles.image}
+                      source={{uri: `${e.image}`}}
+                    />
+                    <Text style={globalStyles.price}>${e.price}</Text>
+                  </View>
+                  {titleHandler(e.title)}
+                </TouchableOpacity>
+              ))}
+            </View>
+          </ScrollView>
+        ) : (
+          <Text style={{alignSelf: 'center', alignContent: 'center'}}>
+            no data found
+          </Text>
+        )}
+      </View>
+    </SafeAreaView>
+  );
 }
 
 const styles = StyleSheet.create({
-    selectedItemView: {
-        justifyContent: 'center',
-        width: '95%',
-        padding: '3.5%',
-        borderRadius: 30,
-        marginHorizontal: 10,
-        height: '40%',
-        backgroundColor: 'lightyellow',
-    },
-    selectedItemUpperView: {
-        flexDirection: 'row',
-        justifyContent: 'flex-start',
-        alignItems: "flex-start",
-        width: '100%',
-        // flex: 4,
-        height: '50%',
-        marginBottom: 8,
-        // backgroundColor: "blue",
-    },
-    selectedItemLeftView: {
-        marginRight: 15,
-        flex: 2,
-        // backgroundColor: "red",
-    },
-    selectedItemRightView: {
-        flex: 6,
-    },
-    selectedItemLowerView: {
-        height: '3%',
-    },
-    selectedItemImage: {
-        height: '100%',
-    },
+  selectedItemView: {
+    justifyContent: 'center',
+    width: '95%',
+    padding: '3.5%',
+    borderRadius: 30,
+    marginHorizontal: 10,
+    height: '40%',
+    backgroundColor: 'lightyellow',
+  },
+  selectedItemUpperView: {
+    flexDirection: 'row',
+    justifyContent: 'flex-start',
+    alignItems: 'flex-start',
+    width: '100%',
+    // flex: 4,
+    height: '50%',
+    marginBottom: 8,
+    // backgroundColor: "blue",
+  },
+  selectedItemLeftView: {
+    marginRight: 15,
+    flex: 2,
+    // backgroundColor: "red",
+  },
+  selectedItemRightView: {
+    flex: 6,
+  },
+  selectedItemLowerView: {
+    height: '3%',
+  },
+  selectedItemImage: {
+    height: '100%',
+  },
 });
--- src/Utils/globalStyles.js
+++ src/Utils/globalStyles.js
@@ -1,88 +1,88 @@
-import { StyleSheet } from "react-native"
-import cusColors from "./colors"
+import {StyleSheet} from 'react-native';
+import cusColors from './colors';
 
 const globalStyles = StyleSheet.create({
-        mainView: {
-        width: '100%',
-        height: '100%',
-        flex: 1,
-        alignItems: "center",
-        // opacity: 0.88,
-    },
-    headerView: {
-        width: "100%",
-        height: "25%",
-        // justifyContent: "center",
-    },
-    headerImage: {
-        width: "100%",
-        height: "100%",
-    },
-    bodyView: {
-        width: "100%",
-        height: "75%",
-        padding: 4,
-        alignItems: "center",
-        justifyContent: "center",
-        backgroundColor: cusColors.offWhite02,
-    },
-    scrollView: {
-        width: "100%",
-        
-        // backgroundColor: "lightyellow",
+  mainView: {
+    width: '100%',
+    height: '100%',
+    flex: 1,
+    alignItems: 'center',
+    // opacity: 0.88,
+  },
+  headerView: {
+    width: '100%',
+    height: '25%',
+    // justifyContent: "center",
+  },
+  headerImage: {
+    width: '100%',
+    height: '100%',
+  },
+  bodyView: {
+    width: '100%',
+    height: '75%',
+    padding: 4,
+    alignItems: 'center',
+    justifyContent: 'center',
+    backgroundColor: cusColors.offWhite02,
+  },
+  scrollView: {
+    width: '100%',
+    // backgroundColor: 'yellowgreen',
+    // alignItems: 'center',
 
-    },
-    categoryHeading: {
-        fontSize: 16,
-        fontWeight: "bold",
-        margin: 12,
-        textDecorationLine: "underline"
-    },
-    cardView: {
-        flexWrap: "wrap",
-        flexDirection: "row",
-        width: "100%",
-        justifyContent: "flex-start",
-        padding: 4,
-        // backgroundColor: "yellowgreen",
-        // opacity: .5,
-    },
-    cardUnit: {
-        width: "20.95%",
-        // width: 85,
-        height: 220,
-        margin: 6,
-        alignItems: "center",
-        justifyContent: "flex-start",
-        backgroundColor: "white",
-    },
-    image: {
-        width: 65,
-        height: 130,
-        // padding: 20,
-        // backgroundColor: "yellow",
-    },
-    price: {
-        fontSize: 14,
-        fontWeight: "700"
-    },
-    text: {
-        fontSize: 14
-    },
-    chipView: {
-        width: 50,
-        // aspectRatio: .5,
-        alignItems: "center",
-        justifyContent: "center",
-        backgroundColor: cusColors.NavyblueColor,
-        margin: 2,
-        padding: 2,
-        borderRadius: 15,
-    },
-    chipText: {
-        color: "white",
-    }
-
-})
+    // backgroundColor: "lightyellow",
+  },
+  categoryHeading: {
+    fontSize: 16,
+    fontWeight: 'bold',
+    margin: 12,
+    textDecorationLine: 'underline',
+  },
+  cardView: {
+    flexWrap: 'wrap',
+    flexDirection: 'row',
+    width: '100%',
+    justifyContent: 'flex-start',
+    padding: 4,
+    // backgroundColor: "yellowgreen",
+    // opacity: .5,
+  },
+  cardUnit: {
+    width: '21.9%',
+    // width: 85,
+    height: 220,
+    margin: 6,
+    alignItems: 'center',
+    justifyContent: 'flex-start',
+    backgroundColor: 'white',
+  },
+  image: {
+    width: 65,
+    height: 130,
+    // padding: 20,
+    // backgroundColor: "yellow",
+  },
+  price: {
+    fontSize: 14,
+    fontWeight: '700',
+  },
+  text: {
+    fontSize: 14,
+  },
+  chipView: {
+    width: 50,
+    // aspectRatio: .5,
+    alignItems: 'center',
+    justifyContent: 'center',
+    backgroundColor: cusColors.NavyblueColor,
+    margin: 2,
+    padding: 2,
+    borderRadius: 15,
+  },
+  chipText: {
+    color: 'white',
+  },
+});
 
 export default globalStyles;
