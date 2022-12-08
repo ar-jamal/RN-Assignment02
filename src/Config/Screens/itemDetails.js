9ca92b69ecfb63883f0a256fa941f1a7b91abbaf
ar-jamal <ar.jamalkarim@gmail.com>
2022-12-08 14:17:17 +0500

    compare error resolved

--- src/Config/Screens/itemDetails.js
+++ src/Config/Screens/itemDetails.js
@@ -1,5 +1,5 @@
 import axios from 'axios';
-import React, {useEffect, useState} from 'react';
+import React, { useEffect, useState } from 'react';
 import {
   Button,
   SafeAreaView,
@@ -14,70 +14,23 @@ import {
 } from 'react-native';
 import globalStyles from '../../Utils/globalStyles';
 
-export default function ItemDetails({Navigation, route}) {
+export default function ItemDetails({ Navigation, route }) {
   const selectedItemId = route.params[0];
   const selectedItem = route.params[1].filter(e => e.id === selectedItemId);
   const sameCatList = route.params[1].filter(e => e.id !== selectedItemId);
 
-<<<<<<< HEAD
-    const selectedItemId = route.params[0]
-    const sameCatList = route.params[1]
-    // console.log(sameCatList)
-    const [selectedState, setSelectedState] = useState({
+  // console.log(sameCatList)
+  const [selectedState, setSelectedState] = useState({
 
-        selectedItem: sameCatList.find(e => e.id === selectedItemId),
-        sameCatListFiltered: sameCatList.filter(e => e.id !== selectedItemId)
-    })
-    // console.log(selectedState.selectedItem)
+    selectedItem: sameCatList.find(e => e.id === selectedItemId),
+    sameCatListFiltered: sameCatList.filter(e => e.id !== selectedItemId)
+  })
+  // console.log(selectedState.selectedItem)
 
-    console.log(selectedState.sameCatListFiltered);
-    console.log(sameCatList);
-
-    const [loader, setloader] = useState(false);
-
-    const cusChip = val => {
-        return (
-            <View style={globalStyles.chipView}>
-                <Text style={globalStyles.chipText}>more</Text>
-            </View>
-        );
-    };
-    const titleHandler = val => {
-        const title = val.length > 12 ? val.slice(0, 12) + '...' : val;
-        return (
-            <View
-                style={{
-                    justifyContent: 'space-between',
-                    backgroundColor: 'yellow',
-                    width: '100%',
-                    flex: 3,
-                    paddingHorizontal: 6,
-                    paddingBottom: 6,
-                    borderRadius: 15
-                }}>
-                <Text style={globalStyles.text}>{title}</Text>
-                {cusChip(val)}
-            </View>
-        );
-    };
-
-    const onSameCatPressHandler = (val) => {
-        console.log('val', val)
-        const selectedItem02 = sameCatList.find(e => e.id === val);
-        const sameCatListFiltered02 = sameCatList.filter(e => e.id !== val);
-        setSelectedState({
-
-            selectedItem: selectedItem02,
-            sameCatListFiltered: sameCatListFiltered02
-        })
-        // console.log(selectedState.selectedItem)
-    }
-    console.log(selectedState.selectedItem)
-=======
-  console.log(selectedItem);
+  console.log(selectedState.sameCatListFiltered);
+  console.log(sameCatList);
 
   const [loader, setloader] = useState(false);
->>>>>>> b61abad47394543c697aef73a132b2db8f928cca
 
   const cusChip = val => {
     return (
@@ -97,12 +50,27 @@ export default function ItemDetails({Navigation, route}) {
           flex: 3,
           paddingHorizontal: 6,
           paddingBottom: 6,
+          borderRadius: 15
         }}>
         <Text style={globalStyles.text}>{title}</Text>
         {cusChip(val)}
       </View>
     );
   };
+
+  const onSameCatPressHandler = (val) => {
+    console.log('val', val)
+    const selectedItem02 = sameCatList.find(e => e.id === val);
+    const sameCatListFiltered02 = sameCatList.filter(e => e.id !== val);
+    setSelectedState({
+
+      selectedItem: selectedItem02,
+      sameCatListFiltered: sameCatListFiltered02
+    })
+    // console.log(selectedState.selectedItem)
+  }
+  console.log(selectedState.selectedItem)
+  console.log(selectedItem);
 
   return (
     <SafeAreaView style={globalStyles.mainView}>
@@ -128,12 +96,11 @@ export default function ItemDetails({Navigation, route}) {
               {selectedItem[0].category}
             </Text>
             <View style={styles.selectedItemView}>
-              {/* <View style={styles.selectedItemUpperView}> */}
               <View style={styles.selectedItemLeftView}>
                 <Image
                   resizeMode="contain"
                   style={styles.selectedItemImage}
-                  source={{uri: `${selectedItem[0].image}`}}
+                  source={{ uri: `${selectedItem[0].image}` }}
                 />
                 <Text style={globalStyles.price}>
                   Price: ${selectedItem[0].price}
@@ -141,12 +108,12 @@ export default function ItemDetails({Navigation, route}) {
               </View>
               <View style={styles.selectedItemRightView}>
                 <Text style={globalStyles.text}>{selectedItem[0].title}</Text>
-                <Text style={{fontSize: 14, fontWeight: '600'}}>
+                <Text style={{ fontSize: 14, fontWeight: '600' }}>
                   Description:
                 </Text>
                 <View
-                 style={{height: 60}}
-                 >
+                  style={{ height: 60 }}
+                >
                   <ScrollView>
                     <Text style={globalStyles.text}>
                       {selectedItem[0].description}
@@ -155,160 +122,67 @@ export default function ItemDetails({Navigation, route}) {
                 </View>
               </View>
             </View>
-            {/* </View> */}
             <View style={[globalStyles.cardView]}>
               {sameCatList.map((e, i) => (
                 <TouchableOpacity
                   style={[globalStyles.cardUnit]}
                   key={i}
                   onpress={() => onSameCatPressHandler(e.id)}>
-                  <View style={{justifyContent: 'space-between', flex: 7}}>
+                  <View style={{ justifyContent: 'space-between', flex: 7 }}>
                     <Image
                       resizeMode="contain"
                       style={globalStyles.image}
-                      source={{uri: `${e.image}`}}
+                      source={{ uri: `${e.image}` }}
                     />
-<<<<<<< HEAD
-                ) : selectedState.sameCatListFiltered.length > 0 ? (
-                    <ScrollView style={globalStyles.scrollView}>
-                        <Text style={globalStyles.categoryHeading}>
-                            {selectedState.selectedItem.category}
-                        </Text>
-                        <View style={styles.selectedItemView}>
-                            <View style={styles.selectedItemUpperView}>
-                                <View style={styles.selectedItemLeftView}>
-                                    <Image
-                                        resizeMode="contain"
-                                        style={styles.selectedItemImage}
-                                        source={{ uri: `${selectedState.selectedItem.image}` }}
-                                    />
-                                </View>
-                                <View style={styles.selectedItemRightView}>
-                                    <Text style={globalStyles.price}>
-                                        ${selectedState.selectedItem.price}
-                                    </Text>
-                                    <Text style={globalStyles.text}>
-                                        {selectedState.selectedItem.title}
-                                    </Text>
-                                </View>
-                            </View>
-                            <ScrollView style={styles.selectedItemLowerView}>
-                                <View>
-                                    <Text style={{ fontSize: 14, fontWeight: '600' }}>
-                                        Description:
-                                    </Text>
-                                    <Text style={globalStyles.text}>
-                                        {selectedState.selectedItem.description}
-                                    </Text>
-                                </View>
-                            </ScrollView>
-                        </View>
-                        <View style={[globalStyles.cardView]}>
-                            {selectedState.sameCatListFiltered.map((e, i) => (
-                                <TouchableOpacity
-                                    style={[globalStyles.cardUnit, { borderRadius: 15 }]}
-                                    key={i}
-                                    onPress={() => onSameCatPressHandler(e.id)}
-
-                                >
-                                    <View style={{ justifyContent: 'space-between', flex: 7 }}>
-                                        <Image
-                                            resizeMode="contain"
-                                            style={globalStyles.image}
-                                            source={{ uri: `${e.image}` }}
-                                        />
-                                        <Text style={globalStyles.price}>${e.price}</Text>
-                                    </View>
-                                    {titleHandler(e.title)}
-                                </TouchableOpacity>
-                            ))
-                            }
-                        </View>
-                    </ScrollView>
-                ) : (
-                    <Text style={{ alignSelf: 'center', alignContent: 'center' }}>
-                        no data found
-                    </Text>
-                )}
-=======
                     <Text style={globalStyles.price}>${e.price}</Text>
                   </View>
                   {titleHandler(e.title)}
                 </TouchableOpacity>
               ))}
->>>>>>> b61abad47394543c697aef73a132b2db8f928cca
-            </View>
-          </ScrollView>
+            </View >
+          </ScrollView >
         ) : (
-          <Text style={{alignSelf: 'center', alignContent: 'center'}}>
+          <Text style={{ alignSelf: 'center', alignContent: 'center' }}>
             no data found
           </Text>
         )}
-      </View>
-    </SafeAreaView>
+      </View >
+    </SafeAreaView >
   );
 }
 
 const styles = StyleSheet.create({
-<<<<<<< HEAD
-    selectedItemView: {
-        justifyContent: 'center',
-        width: '95%',
-        padding: '3.5%',
-        borderRadius: 30,
-        marginHorizontal: 10,
-        height: 170,
-        backgroundColor: 'lightyellow',
-    },
-    selectedItemUpperView: {
-        flexDirection: 'row',
-        justifyContent: 'flex-start',
-        alignItems: "flex-start",
-        width: '100%',
-        // flex: 4,
-        height: '50%',
-        marginBottom: 8,
-        // backgroundColor: "blue",
-    },
-    selectedItemLeftView: {
-        marginRight: 15,
-        flex: 2,
-        // backgroundColor: "red",
-    },
-    selectedItemRightView: {
-        flex: 6,
-    },
-    selectedItemLowerView: {
-        height: '3%',
-    },
-    selectedItemImage: {
-        height: '100%',
-    },
-=======
   selectedItemView: {
+    justifyContent: 'center',
+    width: '95%',
+    padding: '3.5%',
+    borderRadius: 30,
+    marginHorizontal: 10,
+    height: 170,
+    backgroundColor: 'lightyellow',
+  },
+  selectedItemUpperView: {
     flexDirection: 'row',
     justifyContent: 'flex-start',
-    alignItems: 'flex-start',
-    width: '95%',
-    height: '33%',
-    padding: '4%',
-    // paddingBottom: 5,
-    borderRadius: 25,
-    marginHorizontal: 10,
-    marginBottom: 15,
-    backgroundColor: 'lightyellow',
+    alignItems: "flex-start",
+    width: '100%',
+    // flex: 4,
+    height: '50%',
+    marginBottom: 8,
+    // backgroundColor: "blue",
   },
   selectedItemLeftView: {
     marginRight: 15,
-    flex: 2.3,
+    flex: 2,
+    // backgroundColor: "red",
   },
   selectedItemRightView: {
     flex: 6,
-    backgroundColor: "yellow",
+  },
+  selectedItemLowerView: {
+    height: '3%',
   },
   selectedItemImage: {
-    height: '85%',
-    marginBottom: 3,
+    height: '100%',
   },
->>>>>>> b61abad47394543c697aef73a132b2db8f928cca
-});
+})
--- src/Utils/globalStyles.js
+++ src/Utils/globalStyles.js
@@ -1,8 +1,14 @@
-import {StyleSheet} from 'react-native';
-import cusColors from './colors';
+import { StyleSheet } from "react-native"
+import cusColors from "./colors"
 
 const globalStyles = StyleSheet.create({
-        mainView: {
+    mBottomTabStyle: {
+        // justifyContent: "center",
+        // height: 40,
+        // padding: 5,
+        // backgroundColor: "red",
+    },
+    mainView: {
         width: '100%',
         height: '100%',
         flex: 1,
@@ -24,18 +30,18 @@ const globalStyles = StyleSheet.create({
         padding: 4,
         alignItems: "center",
         justifyContent: "center",
-        backgroundColor: cusColors.offWhite02,
+        backgroundColor: cusColors.lighSkyColor,
     },
     scrollView: {
         width: "100%",
-        
+
         // backgroundColor: "lightyellow",
 
     },
     categoryHeading: {
         fontSize: 16,
         fontWeight: "bold",
-        margin: 12,
+        margin: 8,
         textDecorationLine: "underline"
     },
     cardView: {
@@ -48,7 +54,7 @@ const globalStyles = StyleSheet.create({
         // opacity: .5,
     },
     cardUnit: {
-        width: "20.95%",
+        width: "21.95%",
         // width: 85,
         height: 220,
         margin: 6,
